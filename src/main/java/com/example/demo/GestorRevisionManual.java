77cf9a726621a05c3bee5f09d7e770a439eddab4
Lucas Salina <lucassalina69420@gmail.com>
2025-05-19 19:14:40 +0000

    Añadiendo estructura basica y clases de entidad

---
+++ src/main/java/com/example/demo/GestorRevisionManual.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+import java.time.LocalTime;
+
+public class GestorRevisionManual {
+    public void registrarRevisionManual() {
+
+    }
+    
+    public void buscarEventosSismicosNoRevisados() {
+
+    }
+
+    public void buscarEstadoNoRevisadoOAutodetectado() {
+
+    }
+
+    public void ordenarPorFechaHoraOcurrencia() {
+
+    }
+
+    public void bloquearEventoSismicoSeleccionado() {
+
+    }
+
+    public LocalTime getFechaHoraActual() {
+        return LocalTime.now();
+    }
+
+    public void buscarDatosRegistrados() {
+
+    }
+
+    public void ordenarPorEstacionSismologica() {
+
+    }
+
+    public void validarDatosSismicos() {
+
+    }
+    
+    public void actualizarEventoSismicoARechazado() {
+
+    }
+    
+}
---
+++ src/main/java/com/example/demo/PantallaRegistrarRevision.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class PantallaRegistrarRevision {
+    
+    public void habilitarVentana() {
+
+    }
+
+    public void mostrarEventosSismicosYSolicitarSeleccion() {
+
+    }
+    public void tomarSeleccionEventoSismico() {
+
+    }
+
+    public void habilitarOpcionVisualizarMapaEventosYEstacionesInvolucradas() {
+
+    }
+    public void tomarNoVisualizacion() {
+
+    }
+
+    public void solicitarModificacionDatosSismicos() {
+
+    }
+    public void tomarRechazoModificacion() {
+
+    }
+    
+}
