f3dc4438613ee4c3166aced09caba9053213494f
draguncarreras <draguncarreras@gmail.com>
2025-05-20 19:11:14 +0000

    Agregando el esbozo del  GestorRevisionManual según el Modelo de Análisis

---
+++ src/main/java/com/grupo7/application/controller/GestorRevisionManual.java
@@ -0,0 +1,72 @@
+package com.grupo7.application.controller;
+
+// Dependencies
+import com.grupo7.application.entity.EventoSismico;
+
+import com.grupo7.application.entity.Estado;
+
+import java.sql.Date;
+
+public class GestorRevisionManual {
+    private Date fechaHoraOcurrenciaEventoSismico;
+    private String latitudEpicentroEventoSismico;
+    private String longitudEpicentroEventoSismico;
+    private String longitudHipocentroEventoSismico;
+    private String seleccionEventoSismico;
+    public boolean opVisualizacion;
+    public boolean opRechazoModificacion;
+
+    public void registrarRevisionManual() {
+        // TODO: implementar
+    }
+
+    public void buscarEventosSismicosNoRevisados() {
+        // TODO: implementar
+    }
+
+    public void buscarEstadoNoRevisadosOAutodetectado() {
+        // TODO: implementar
+    }
+
+    public void ordenarPorFechaHoraOcurrencia() {
+        // TODO: implementar
+    }
+
+    public void tomarSeleccionEventoSismico() {
+        // TODO: implementar
+    }
+
+    public void bloquearEventoSismicoSeleccionado() {
+        // TODO: implementar
+    }
+
+    public void getFechaHoraActual() {
+        // TODO: implementar
+    }
+
+    public void buscarDatosRegistrados() {
+        // TODO: implementar
+    }
+
+    public void ordenarPorEstacionSismologica() {
+        // TODO: implementar
+    }
+
+    public void tomarNoVisualizacion() {
+        // TODO: implementar
+    }
+
+    public void tomarRechazoModificacion() {
+        // TODO: implementar
+    }
+
+    public void validarDatosSismicos() {
+        // TODO: implementar
+    }
+
+    public void actualizarEventoSisimicoARechazado() {
+        // TODO: implementar
+    }
+
+
+}
