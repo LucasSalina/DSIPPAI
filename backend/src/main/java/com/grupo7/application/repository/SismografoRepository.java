68c48dbe7dfc71887caf0795c5e1933e639b0baf
alumno <alumno@GI.FRC.UTN.EDU.AR>
2025-05-22 11:32:43 -0300

    arreglo general de la bd

--- backend/src/main/java/com/grupo7/application/repository/SismografoRepository.java
+++ backend/src/main/java/com/grupo7/application/repository/SismografoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SismografoRepository extends JpaRepository<Sismografo, Long> {
+public interface SismografoRepository extends JpaRepository<Sismografo, String> {
     // Métodos personalizados si los necesitas
 }
