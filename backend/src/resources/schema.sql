ed441b95be11161efa992523cc033637b2e1a3ad
draguncarreras <draguncarreras@gmail.com>
2025-05-22 12:55:09 +0000

    Agregando la configuración de H2 y JPA para trabajar con una base de datos en memoria.

--- backend/src/resources/schema.sql
+++ backend/src/resources/schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS TIPO_DE_DATO (
 CREATE TABLE IF NOT EXISTS ESTACION_SISMOLOGICA (
   codigo_estacion       TEXT PRIMARY KEY,
   documento_cert_adq    TEXT,
-  fecha_solicitud_cert  TEXT,
+  fecha_solicitud_cert  DATE,
   latitud               REAL,
   longitud              REAL,
   nombre                TEXT,
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS ESTACION_SISMOLOGICA (
 -- Tablas transaccionales
 CREATE TABLE IF NOT EXISTS EVENTO_SISMICO (
   id_evento_sismico       INTEGER PRIMARY KEY AUTOINCREMENT,
-  fecha_hora_fin          TEXT,
-  fecha_hora_ocurrencia   TEXT,
+  fecha_hora_fin          DATETIME,
+  fecha_hora_ocurrencia   DATETIME,
   latitud_epicentro       REAL,
   longitud_epicentro      REAL,
   latitud_hipocentro      REAL,
@@ -63,8 +63,8 @@ CREATE TABLE IF NOT EXISTS CAMBIO_ESTADO (
   id_cambio               INTEGER PRIMARY KEY AUTOINCREMENT,
   id_evento               INTEGER REFERENCES EVENTO_SISMICO(id_evento_sismico),
   id_estado               INTEGER REFERENCES ESTADO(id_estado),
-  fecha_hora_inicio       TEXT,
-  fecha_hora_fin          TEXT,
+  fecha_hora_inicio       DATETIME,
+  fecha_hora_fin          DATETIME,
   es_estado_actual        INTEGER,
   sos_autodetectado       INTEGER,
   sos_pendiente_revision  INTEGER
@@ -79,15 +79,15 @@ CREATE TABLE IF NOT EXISTS SISMOGRAFO (
 
 CREATE TABLE IF NOT EXISTS MUESTRA_SISMICA (
   id_muestra                INTEGER PRIMARY KEY AUTOINCREMENT,
-  fecha_hora_muestra        TEXT,
+  fecha_hora_muestra        DATETIME,
   id_detalle_muestra        INTEGER REFERENCES DETALLE_MUESTRA_SISMICA(id_detalle_muestra_sismica)
 );
 
 CREATE TABLE IF NOT EXISTS SERIE_TEMPORAL (
   id_serie                       INTEGER PRIMARY KEY AUTOINCREMENT,
   condicion_alarma               TEXT,
-  fecha_hora_inicio_reg_muestreo TEXT,
-  fecha_hora_registros           TEXT,
+  fecha_hora_inicio_reg_muestreo DATETIME,
+  fecha_hora_registros           DATETIME,
   frecuencia_muestreo            REAL,
   id_muestra_sismica             INTEGER REFERENCES MUESTRA_SISMICA(id_muestra),
   id_sismografo                  TEXT    REFERENCES SISMOGRAFO(identificador)
